c1381818cc853b16527b21994fc4122088c04170
村上舜平 <wushumenmenmen2@gmail.com>
2020-03-12 18:30:27 +0900

    second commit

--- handle_name_serch.py
+++ handle_name_serch.py
@@ -5,6 +5,7 @@ import json
 import openpyxl
 import re
 from bs4 import BeautifulSoup as bs
+import time
 
 
 ##ローカルにあるhandle.xlsxにアクセスする（一列目にハンドル名入力)
@@ -14,6 +15,7 @@ sheet2=wb2['Sheet1']
 
 ##excelの一列目のハンドルを取得し、三列目に出力する
 for i in range(0,sheet2.max_row):
+    time.sleep(1)
     print(sheet2.cell(row=i+1,column=1).value)    
     req = requests.get("https://www.google.com/search?q=" + sheet2.cell(row=i+1,column=1).value)
     req = req.text
@@ -22,8 +24,13 @@ for i in range(0,sheet2.max_row):
     if(tags[0].find("div",class_="BNeawe vvjwJb AP7Wnd") != None):
         ##title = tags[0].find("div", class_="BNeawe vvjwJb AP7Wnd").string.split('(@')[0]
         title2 = tags[0].find("div", class_="BNeawe vvjwJb AP7Wnd").string 
-        ##if title2 in "(@":
-        ##    title2 = title2.split('(@')[0]
+        if "(@" in title2:
+            title2 = title2.split('(@')[0]
+        else:
+            if "- Twitter" in title2:
+                title2 = title2.split('-')[0]
+            if "✓" in title2:
+                title2 = title2.split('✓')[0]
         print(title2)
         sheet2.cell(row=i+1,column=3).value= title2
         wb2.save('/Users/days/Documents/google_search/handle.xlsx')
